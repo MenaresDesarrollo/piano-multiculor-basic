b53353301cbc98c022c6429f8ce7d5d05587709c
Alonso Painevilo Albino <alonso.painevilo@labocenter.cl>
2023-03-03 15:40:11 -0300

    unico commit con el mini proyecto completo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Piano';
+
+  aplicarSonido(numero: number): void{
+    const audio = new Audio();
+    audio.src = '../assets/sonidos/note' + numero + '.wav';
+    audio.load();
+    audio.play();
+  }
 }
